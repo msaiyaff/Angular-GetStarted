558d3c90d2ee747f3da94505f7e330823a7bd4f5
Mohamad Saiyaff <MohamadS@99x.lk>
2016-10-24 18:23:38 +0530

    initial folder structure for get started with angualar js 2

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular App</h1> <p> This is some awesome stuff </p>'
+})
+export class AppComponent { }
